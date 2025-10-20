d0a680ab7427719519f7433229602da18f4dad73
HighEnd2005 <dinhhien1652005@gmail.com>
2025-10-20 18:59:00 +0700

    remove Cinema + Hola Cinema folder and add add_movie and movie_management again

---
+++ src/java/Models/Movie.java
@@ -0,0 +1,156 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author dinhh
+ */
+public class Movie {
+    private int movieId;
+    private String title;
+    private String rating;
+    private int durationMinutes;
+    private String language;
+    private LocalDate releaseDate;
+    private String status;
+    private String posterUrl;
+    private String trailerUrl;
+    private String directorName;
+    private String description;
+    private List<Integer> genreIds;
+    private List<Integer> actorIds;
+
+    public Movie() {}
+
+    public Movie(int movieId, String title, String rating, int durationMinutes, String language, LocalDate releaseDate,
+                 String status, String posterUrl, String trailerUrl, String directorName, String description,
+                 List<Integer> genreIds, List<Integer> actorIds) {
+        this.movieId = movieId;
+        this.title = title;
+        this.rating = rating;
+        this.durationMinutes = durationMinutes;
+        this.language = language;
+        this.releaseDate = releaseDate;
+        this.status = status;
+        this.posterUrl = posterUrl;
+        this.trailerUrl = trailerUrl;
+        this.directorName = directorName;
+        this.description = description;
+        this.genreIds = genreIds;
+        this.actorIds = actorIds;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getTrailerUrl() {
+        return trailerUrl;
+    }
+
+    public void setTrailerUrl(String trailerUrl) {
+        this.trailerUrl = trailerUrl;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public List<Integer> getActorIds() {
+        return actorIds;
+    }
+
+    public void setActorIds(List<Integer> actorIds) {
+        this.actorIds = actorIds;
+    }
+    
+
+}
